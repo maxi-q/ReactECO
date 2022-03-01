24d754ebc412b5fe5c60b5af225ba12b553b4318
maxi-q <maximkabust@gmail.com>
2022-03-01 15:38:46 +0300

    -_- шото с постом не так

--- src/config/Requests.js
+++ src/config/Requests.js
@@ -1,19 +1,24 @@
 import React from "react";
-import axios from 'axios';
 
 export default async function httpPost(name, password, navigate) {
 
-    const body = JSON.stringify({
-        name: name,
-        password: password
-    });
-    axios
-        .post('http://127.0.0.2:5000/api/registration', body)
-        .then(response => {
-            console.log(response)
+    const options = {
+        method: "POST",
+        mode: 'no-cors',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({
+            'name': name,
+            'password': password
         })
-        .catch(error =>{
-            console.log(error)
-        })
+    }
+    let response = await fetch('http://127.0.0.2:5000/api/registration', options);
+    if (response.ok) { // если HTTP-статус в диапазоне 200-299
+                       // получаем тело ответа (см. про этот метод ниже)
+        let json = await response.json();
+        console.log(json)
+    } else {
+        console.log(await response.status)
+    }
+
     navigate('/MainWindow');
 }
