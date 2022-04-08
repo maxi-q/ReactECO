0d3071af8765bf8e0ea39cd7e615c901308023e8
maxi-q <maximkabust@gmail.com>
2022-04-08 22:25:14 +0300

    kvant

---
+++ src/components/Revard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Stack, Image } from 'react-bootstrap'
+
+const Revard = (props) => {
+    return (
+        <div className="Histori">
+            <Stack direction="horizontal" gap={0}>
+                <Image src={props.value.avatar} className="RevardAva"/>
+                <p className="RevardText">
+                    <h5>{props.value.title}</h5>
+                    {props.value.body}
+                </p>
+            </Stack>
+        </div>
+    )
+};
+export default Revard
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Image, Stack } from 'react-bootstrap'
+import Revard from '../components/Revard'
 
 const Profile = () => {
+    const img = "https://i.pinimg.com/originals/4b/ee/62/4bee62fe59c6c7ff150186a4318e3180.jpg";
+    const ava = "https://funart.pro/uploads/posts/2021-07/1627167714_14-funart-pro-p-kot-v-pidzhake-zhivotnie-krasivo-foto-17.jpg";
+    const revards = 10;
     return (
-        <div style={{backgroundColor:"rgb(32,202,135)"}}>
-            Profile
+        <div>
+            <Image src={img} className="ProfileImage"/>
+            <div className="ProfileBlock">
+                <Stack direction="horizontal" gap={3}>
+                    <Image src={ava} className="Avatar"/>
+                    <p className="NameProfile">
+                        <h3>Никнейм</h3>
+                        Достижения: {revards}/42
+                    </p>
+                </Stack>
+                <div className="HistoryBlock">
+                    <h4>История:</h4>
+                    <div className="Histories">
+                        <Revard value={{avatar: ava, title:"Покормить кота", body: "Найти кота в карточках и покормитт его силой воображения"}}/>
+                        <Revard value={{avatar: ava, title:"Поймать слона", body: "Найти слона и поймать его сетью для аллигаторов"}}/>
+                        <Revard value={{avatar: ava, title:"Найти оленя", body: "Посмотреть на оленя"}}/>
+                        <Revard value={{avatar: ava, title:"Поймать себя", body: "Залезть в мешок"}}/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
